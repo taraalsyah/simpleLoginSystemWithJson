82993e9c5ea972643ac1c3320d9c25123b617358
Tara Alsyah <58825091+taraalsyah@users.noreply.github.com>
2022-03-15 09:46:40 +0700

    validateLogin.py
    Validate login with json as database.

---
+++ validateLogin.py
@@ -0,0 +1,20 @@
+import json
+def write_json(data,filename="dictLatihan5.json"):
+    with open(filename,"w") as f:
+        json.dump(data,f,indent = 4)
+with open ("dictLatihan5.json") as json_file:
+    data = json.load(json_file)
+    y = {"nama":"tara","password":"tara123"}
+    data.append(y)
+write_json(data)
+u = 'tara'
+p = 'tara123'
+x=len(data)
+for i in range(len(data)):
+    databaru = data[i].values()
+    if u in databaru and p in databaru:
+        print('benar')
+        break
+    x=x-1
+if x == 0:
+    print('salah')
